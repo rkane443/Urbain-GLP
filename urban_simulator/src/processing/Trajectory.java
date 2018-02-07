990ac9e4bc244b61c937b6669dd3cd1877bafa2d
Dartron François <35767615+dartronfl@users.noreply.github.com>
2018-02-07 18:12:40 +0100

    Update Trajectory.java

--- urban_simulator/src/processing/Trajectory.java
+++ urban_simulator/src/processing/Trajectory.java
@@ -6,6 +6,17 @@ public class Trajectory {
 
 	ArrayList <Route> paths =new ArrayList();
 	
+	public void add(Route path)throws RouteAlreadyExistsException {
+		if (paths.contains(path)) {
+			throw new RouteAlreadyExistsException(path);
+		} else {
+			paths.add(path);
+		}
+	}
+	
+	public boolean contains(Route path) {
+		return paths.contains(path);
+	}
 	
 	
 }
