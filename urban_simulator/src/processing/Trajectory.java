f72329a56b707c0446338b59e3ab5693a04b1cc9
Dartron François <35767615+dartronfl@users.noreply.github.com>
2018-02-07 18:18:25 +0100

    Update Trajectory.java
    ajout commentaire

--- urban_simulator/src/processing/Trajectory.java
+++ urban_simulator/src/processing/Trajectory.java
@@ -2,10 +2,16 @@ package processing;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author François-Laurent
+ *
+ */
 public class Trajectory {
 
 	ArrayList <Route> paths =new ArrayList();
 	
+	// ajout de chemin
 	public void add(Route path)throws RouteAlreadyExistsException {
 		if (paths.contains(path)) {
 			throw new RouteAlreadyExistsException(path);
