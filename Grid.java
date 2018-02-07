28dff43e1c5545799477a044501d76a82971ab14
JavaVivi <34030149+JavaVivi@users.noreply.github.com>
2018-02-07 11:48:42 +0100

    Add files via upload

---
+++ Grid.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package data;
+
+/**
+ * @author andro
+ *
+ */
+public class Grid {
+	
+	private Case cases = new Case(Cases);
+	private Particulars particulars = new Particulars(Particulars);
+	public Case getCases() {
+		return cases;
+	}
+
+}
