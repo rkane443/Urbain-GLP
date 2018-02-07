7eeae78c22818cbdb9a5d7be23a83423f8f9f56c
JavaVivi <34030149+JavaVivi@users.noreply.github.com>
2018-02-07 11:50:00 +0100

    Add files via upload

---
+++ Particulars.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package data;
+
+/**
+ * @author andro
+ *
+ */
+public class Particulars {
+	private int X;
+	private int Y;
+	
+	public Particulars (int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+	}
+	public int getX () {
+		return X;
+	}
+	public int getY () {
+		return Y;
+	}
+}
