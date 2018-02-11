7d10eb7578619a46848de737b4a3792fc21ca5e5
Dartron François <35767615+dartronfl@users.noreply.github.com>
2018-02-11 21:55:27 +0100

    v1
    Nouvelle version sans bug pour les déplacements

--- urban_simulator/src/processing/Route.java
+++ urban_simulator/src/processing/Route.java
@@ -10,77 +10,76 @@ import data.Particulars;
 
 public class Route {
 	private String name;
+	private ArrayList<Point> journey =new ArrayList<Point>();
+	String  move;
 	
-	
-	public Route(){}
-	
-	public void down(int y) {
-		y++;
+	public Route(String name, String paths,ArrayList<Point> pt){
+		this.name=name;
+		this.move=paths;
+		this.journey=pt;
 		
 	}
 	
-	public void up(int y) {
-		y--;
-	}
-		
-	public void left(int x) {
-		x--;
-	}
-	
-	public void right(int x) {
-		x++;
-	}
-	
-	public void trip(String[] options,ArrayList<Point> pt,Particulars character) {
+	public void trip(Particulars character) {
 		
 		int i;
 		int x, y;
-		x=character.getX();
-		y=character.getY();
+		x =character.getX();
+		y =character.getY();
 		
-		for (i=0; i < options.length; i++) {
+		String [] options;
+		
+		options= move.split("-");
+		int lg  = options.length;
+		
+		
+		for (i=0; i < lg ; i++) {
 			String argument= options[i];
 			
-			  Point waypoint=pt.get(i);
-			  Point end =pt.get(i+1);
+			  Point waypoint=journey.get(i);
+			  Point end =journey.get(i+1);
+			  
 			  int wx = waypoint.getX();
 			  int wy = waypoint.getY();
+			  
 			  int ex = end.getX();  
 			  int ey = end.getY();
 			  
 			  
-			// si on met L déplacement à gauche
-			if (argument.compareToIgnoreCase("L") == 0 ){
-				  
-				while (wx != ex ) {
-				left(x);	
+			  System.out.println("Depart");
+			  if (argument.compareToIgnoreCase("L") == 0 ){
+				  int x1;
+				  System.out.println("L");
+				for(x1=wx ; x1 <ex ; x1++ ) {
+					character.setX(x-1);
 				}
-				character.setX(x);
+				
+		
 			  }
 			
-			// si on met L déplacement à droite
-			else if (argument.compareToIgnoreCase("R") == 0) {
-				while (wx != ex ) {
-				right(x);	
-				}
-				character.setX(x);
-			  }
-			
-			// si on met L déplacement en haut
-			else if (argument.compareToIgnoreCase("U") == 0) {
-				while (wy != ey ) {
-				up(y);	
-				}
-				character.setY(y);
-			  }
-			
-			// si on met D déplacement en bas
-			else if (argument.compareToIgnoreCase("D") == 0) {
-					while (wy != ey ) {
-					down(y);	
+			  else if (argument.compareToIgnoreCase("R") == 0) {
+				  int x2;
+				  System.out.println("R");
+				  for(x2=wx ; x2 <ex ; x2++ ){
+				character.setX(x+1);
 					}
-					character.setY(y);
-				  }
+				}
+			  
+			  else if (argument.compareToIgnoreCase("U") == 0) {
+				   int y1;
+				   System.out.println("U");
+				   for(y1=wy ; y1 <ey ; y1++ ) {
+					character.setY(y-1);		
+					}
+				}
+			  
+			  else if (argument.compareToIgnoreCase("D") == 0) {
+				  int y2;
+				  System.out.println("D");
+				  for(y2=wy ; y2 <ey ; y2++ ) {	
+					character.setY(y+1);
+					}
+			  	}
 			  else {
 				  
 				  System.err.println("Lack of information");
@@ -101,4 +100,7 @@ public class Route {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	
 }
